a60400254e415514db6f6ec12fc8c22ca702a6ce
willokans <willokans@outlook.com>
2026-06-24 14:55:54 +0100

    Fix python_ollama file to point to right model and dependency

--- python_ollama.py
+++ python_ollama.py
@@ -1,21 +1,21 @@
 from langchain_ollama import ChatOllama
-from langchain.core.messages import SystemMessage, HummanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 
 
 # initiate the model
 llm = ChatOllama(
-    model="llama3.2",
+    model="llama3.2:3b",
     temperature=0,
 )
 
 # initiate the 'message' object
 message = [
     SystemMessage("Act like a Piriate"),
-    HummanMessage("What is the weather like today?")
+    HumanMessage("What is the weather like today?"),
 ]
 
 # execute the model (with message)
 result = llm.invoke(message);
 
 # Show result in the screen
-print(result)
+print(result.content)
